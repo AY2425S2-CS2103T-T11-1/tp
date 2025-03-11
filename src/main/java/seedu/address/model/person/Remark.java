aecb4abe3377fe128b761c4998448893ea3b92f0
YosieSYX <e1300499@u.nus.edu>
2025-03-11 15:33:26 +0800

    Add Checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -4,21 +4,38 @@ import static java.util.Objects.requireNonNull;
 
 /**
  * Represents a Person's remark in the address book.
- * Guarantees: immutable; is always valid
+ * A {@code Remark} is immutable and always valid.
  */
 public class Remark {
+    /** The remark text associated with a person. */
     public final String value;
 
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark The remark text. Must not be null.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
     }
 
+    /**
+     * Returns the string representation of the remark.
+     *
+     * @return The remark text.
+     */
     @Override
     public String toString() {
         return value;
     }
 
+    /**
+     * Checks if this remark is equal to another object.
+     *
+     * @param other The other object to compare.
+     * @return {@code true} if the other object is a {@code Remark} with the same value, otherwise {@code false}.
+     */
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
@@ -26,6 +43,11 @@ public class Remark {
                 && value.equals(((Remark) other).value)); // state check
     }
 
+    /**
+     * Returns the hash code of this remark.
+     *
+     * @return The hash code value for this remark.
+     */
     @Override
     public int hashCode() {
         return value.hashCode();
