5a7276a37f8cfc3bc1c57de7b5956586bd71073f
Shashwat Chandra <shashwat@Shashwats-MacBook-Air.local>
2025-03-26 17:07:38 +0800

    Test link extraction

--- src/test/java/seedu/address/model/person/LinkTest.java
+++ src/test/java/seedu/address/model/person/LinkTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 public class LinkTest {
@@ -35,6 +37,20 @@ public class LinkTest {
         assertFalse(Link.isValidLink("www.nusmods.com/timetable/sem-2/share?CS2101")); // missing https
         // valid link
         assertTrue(Link.isValidLink("https://nusmods.com/timetable/sem-2/share?CS2101"));
+    }
+
+    @Test
+    public void extractCodes() {
+        // assertTrue(true);
+        Set<String> codes =
+            Link.extractCodes("https://nusmods.com/timetable/sem-2/share?CP4101=&CS2101=&CS2103T=LEC:G12");
+        assertTrue(codes.contains("CP4101"));
+        assertTrue(codes.contains("CS2101"));
+        assertTrue(codes.contains("CS2103T"));
+        assertTrue(codes.size() == 3);
+        codes =
+            Link.extractCodes("https://nusmods.com/timetable/st-ii/share?AH3550=&CS1010E=TUT:05,SEC:1&MA3289=");
+        assertTrue(codes.size() == 3);
     }
 
     @Test
