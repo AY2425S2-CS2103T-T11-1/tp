935e210ab2017170b4989f0515db29a36b3f19d7
YosieSYX <e1300499@u.nus.edu>
2025-03-27 19:05:28 +0800

    Add logs

--- src/main/java/seedu/address/logic/commands/FindModCommand.java
+++ src/main/java/seedu/address/logic/commands/FindModCommand.java
@@ -7,6 +7,8 @@ import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.ModContainsKeywordsPredicate;
 
+import java.util.logging.Logger;
+
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
@@ -20,18 +22,26 @@ public class FindModCommand extends Command {
             + "Parameters: MODULE_CODE [MORE_MODULE_CODES]...\n"
             + "Example: " + COMMAND_WORD + "CS2100 CS2103T";
 
+    private static final Logger logger = Logger.getLogger(FindModCommand.class.getName());
+
     private final ModContainsKeywordsPredicate predicate;
 
     public FindModCommand(ModContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
+        logger.info("FindModCommand created with predicate: " + predicate);
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        logger.info("Executing FindModCommand with predicate: " + predicate);
+
         model.updateFilteredPersonList(predicate);
-        return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+
+        String resultMessage = String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size());
+        logger.info("Command executed successfully, " + model.getFilteredPersonList().size() + " persons found.");
+
+        return new CommandResult(resultMessage);
     }
 
     @Override
@@ -46,7 +56,10 @@ public class FindModCommand extends Command {
         }
 
         FindModCommand otherFindCommand = (FindModCommand) other;
-        return predicate.equals(otherFindCommand.predicate);
+        boolean isEqual = predicate.equals(otherFindCommand.predicate);
+        logger.fine("Checking equality between FindModCommand objects: " + isEqual);
+
+        return isEqual;
     }
 
     @Override
